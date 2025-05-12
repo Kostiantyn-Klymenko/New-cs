8255bf6d7cd23719445aa57d8b653a8a62dc8803
Kostiantyn-Klymenko <agent.cook.ok@gmail.com>
2025-05-12 22:12:32 +0400

    Converted to GUI but YOUR pomo cod dosen't work for now

--- wORK1.py
+++ wORK1.py
@@ -1,224 +1,252 @@
-from work_func import separation_line, show_products, show_basket, total, your_d_code, products, prices, num_items
+from work_func import show_products, show_basket, total, your_d_code, add_to_basket, remove_from_basket
 import random
 from tkinter import *
 total_store=[0]
 
 
+def show_frame(frame):
+    frame.tkraise()
 
-def main():
-    separation_line()
-    print('''User Functions:
-        » Write \"Add\" to add the product to the basket.
-        » Write \"Look\" to view products in the basket.
-        » Write \"Showe\" to view products available.
-        » Write \"Buy\" to make the purchase.
-        » Write \"Remove\" to remove the products from the basket.''')
+
+def move_if_done():
+    name = entry_user_name.get().strip()
+    if name == "":
+        label1.config(text="Error, please enter your name")
+    else:
+        label2.config(text=f"Hi {name}\nWould you like to make a purchase?\nYes or No: ")
+        label6.config(text=f"Ok {name}\nWould you like to apply coupon?")
+        show_frame(frame_buy_choice)
+
+
+def yes_move():
+    show_frame(frame_main)
+
+
+def add():
+    add_basket = Toplevel()
+    add_basket.title("Add")
+    add_basket.geometry("300x200")
+
+    entry_product = Entry(add_basket, bg='lightgray')
+    entry_product.pack()
+    entry_quantity = Entry(add_basket, bg='lightgray')
+    entry_quantity.pack()
+
+    def get_message():
+        result = add_to_basket(entry_product.get().lower(), int(entry_quantity.get()) )
+        label_add.config(text=result)
+        label_basket.config(text=show_basket())
     
-    out_value=False
-    while out_value==False:
-        user_request=str(input("Write here: ")).lower()   
-        while True:
-            if user_request in ["add"]:
-                user_request2=str(input("What do you want to add? ")).lower()          
-                while True:
-                    if user_request2 in products:
-                        user_request3=int(input(f"How much {user_request2.capitalize()}s do you want to add? "))
-                        num_items[products.index(user_request2)] += user_request3
-                        break
-                    else:
-                        print("Error misinput.")
-                        user_request2=str(input("Please enter valid product: ")).lower()
-                break
-            elif user_request in ["remove"]:
-                user_request2=str(input("What do you want to remove? ")).lower()          
-                while True:
-                    if user_request2 in products:
-                        user_request3=int(input(f"How much {user_request2.capitalize()}s do you want to remove? "))
-                        while True:
-                            if num_items[products.index(user_request2)] >= user_request3:
-                                num_items[products.index(user_request2)] -= user_request3
-                                break
-                            else:
-                                print("Error you are removing more than you have renter the command.")
-                                break
-                        break
-                    else:
-                        print("Error misinput.")
-                        user_request2=str(input("Please enter valid product: ")).lower()
-                break
-            elif user_request in ["look"]:
-                print("Ok opening basket...")
-                separation_line()
-                show_basket()
-                break
-            elif user_request in ["showe"]:
-                print("Ok showing all products...")
-                separation_line()
-                show_products()
-                break
-            elif user_request=="buy" and total()<=0:
-                print("Error you have nothing to purchase renter the command.")
-                break
-            elif user_request=="buy" and total()!=0:
-                print("Ok...")
-                separation_line()
-                buying_choice=str(input("Would you like to apply coupon? \nYes or No: ")).lower()
-                while True:
-                    if buying_choice in ["yes"]:
-                        #calls discount function
-                        discount()
-                        out_value=True
-                        break
-                    elif buying_choice in ["no"]:
-                        print("Ok...")
-                        #sets total_final to total if no discoun applied 
-                        total_store[0]=total()
-                        out_value=True
-                        break
-                    else:
-                        print("Error, invalid input")
-                        buying_choice=str(input("Please enter Yes or No: ")).lower()
-                separation_line()
-                break
-            else:
-                print("Error misinput.")
-                user_request=str(input("Please enter valid command: ")).lower()
-                
+    button_add = Button(add_basket, text="Add", command=get_message)
+    button_add.pack()
+
+    label_add = Label(add_basket, text="Add product")
+    label_add.pack()
+
+    button_close = Button(add_basket, text="Close", command=add_basket.destroy)
+    button_close.pack(side=BOTTOM)
+
+def remove():
+    remove_basket = Toplevel()
+    remove_basket.title("Remove")
+    remove_basket.geometry("300x200")
+
+    entry_product = Entry(remove_basket, bg='lightgray')
+    entry_product.pack()
+    entry_quantity = Entry(remove_basket, bg='lightgray')
+    entry_quantity.pack()
+
+    def get_message():
+        result = remove_from_basket(entry_product.get().lower(), int(entry_quantity.get()) )
+        label_add.config(text=result)
+        label_basket.config(text=show_basket())
+    
+    button_add = Button(remove_basket, text="Remove", command=get_message)
+    button_add.pack()
+
+    label_add = Label(remove_basket, text="Remove product")
+    label_add.pack()
+
+    button_close = Button(remove_basket, text="Close", command=remove_basket.destroy)
+    button_close.pack(side=BOTTOM)
+
+def buy():
+    if total() == 0:
+        label_no_buy.config(text="Error, you have nothing to purchase")
+    else:   
+        total_store[0] = total()
+        show_frame(frame_buy_choice2)
+
+
+def yes_move2():
+    show_frame(frame_discount)
+
+def no_move2():
+    label9.config(text=f"Final bill: {total_store[0]}$")
+    show_frame(frame_end)
 
 
 def discount():
+    user_cod = entry_discount.get().strip()
     total_d=total()
-    separation_line()
-    print("Your promo cod:")
-    print("\n")
-    separation_line()
-    print("⟫ -50% promo code: HALF")
-    separation_line()
-    print("\n")
-    separation_line()
-    print("⟫ -???% promo code: RANDOM")
-    separation_line()
-    print("\n")
-    separation_line()
-    print("⟫ your own % promo code: YOUR")
-    separation_line()
-    print("\n")
-    separation_line()
-    user_cod=str(input("Write your promo cod here: "))
-    while True:
-        if user_cod=="HALF":
-            total_d*=0.5
-            break
-        elif user_cod=="RANDOM":
-            random_num=round(random.uniform(0.0, 1.0), 3)
-            total_d*=random_num
-            break
-        elif user_cod=="YOUR":
-            total_d=your_d_code()
-            break
-        else:
-            print("Error, invalid promo cod")
-            user_cod=str(input("Please enter valid promo cod: "))
+    if user_cod=="HALF":
+        total_d*=0.5
+        show_frame(frame_end)
+    elif user_cod=="RANDOM":
+        random_num=round(random.uniform(0.0, 1.0), 3)
+        total_d*=random_num
+        show_frame(frame_end)
+    #elif user_cod=="YOUR":
+        '''your_promo = Toplevel()
+        your_promo.title("YOUR promo cod")
+        your_promo.geometry("300x200")
+
+        entry_percentag = Entry(your_promo, bg='lightgray')
+        entry_percentag.pack()
+
+        def get_message():
+            global total_d
+            percentage, result = your_d_code(int(entry_percentag.get()))
+            label_promo.config(text = result)
+            total_d *= percentage
+        
+        
+        button_apply = Button(your_promo, text="Apply", command=get_message)
+        button_apply.pack()
+
+        label_promo = Label(your_promo, text="")
+        label_promo.pack()
+
+        button_close = Button(your_promo, text="Close", command=your_promo.destroy)
+        button_close.pack(side=BOTTOM)
+
+        show_frame(frame_end)'''
+    else:
+        label_discount.config(text="Error, invalid promo cod")
     #sets total_final to rounded total_d if discoun alied 
     total_store[0] = round(total_d, 2)
-
-def move_if_done():
-    if entry_user_name.get().strip == "":
-        label2.config(text="Error, please enter your name")
-    else:
-        window_start.destroy()
-        window_buy_choice.mainloop()
-
-def yes_move():
-    window_buy_choice.destroy()
-    window_main.mainloop()
-
-def add():
-    window_main.destroy()
-
-def remove():
-    window_main.destroy()
-
-def buy():
-    window_main.destroy()
-
+    label9.config(text=f"Final bill: {total_store[0]}$")
     
 
 window_app = Tk()
 window_app.title("Shop")
-window_app.geometry("1200x800")
+window_app.geometry("900x600")
 
 
 frame_start = Frame(window_app)
 frame_buy_choice = Frame(window_app)
 frame_main = Frame(window_app)
+frame_buy_choice2 = Frame(window_app)
+frame_discount = Frame(window_app)
+frame_end = Frame(window_app)
+
+
+for frame in (frame_start, frame_buy_choice, frame_main, frame_buy_choice2, frame_discount, frame_end):
+    frame.grid(row=0, column=0, sticky='nsew')
+
 
 #Start frame
 label1= Label(frame_start, text="Please enter your name for personalised greeting.")
-label1.pack()
-entry_user_name = Entry(frame_start)
-entry_user_name.pack()
+label1.pack(side=TOP)
+entry_user_name = Entry(frame_start, bg='lightgray')
+entry_user_name.pack(side=TOP)
 button_name = Button(frame_start, text="Enter", command=move_if_done)
-button_name.pack()
+button_name.pack(side=TOP)
 
 
 #Buy choice frame
-label2= Label(frame_buy_choice, text=f"Hi {entry_user_name.get()}\nWould you like to make a purchase?\nYes or No: ")
-label2.pack()
+label2= Label(frame_buy_choice, text="")
+label2.pack(side=TOP)
 button_yes = Button(frame_buy_choice, text="Yes", command=yes_move)
-button_yes.pack()
+button_yes.pack(side=TOP)
 button_no = Button(frame_buy_choice, text="No", command=window_app.destroy)
-button_no.pack()
+button_no.pack(side=TOP)
 
 
 #Main frame
 label3= Label(frame_main, text='''User Functions:
-        » Write \"Add\" to add the product to the basket.
-        » Write \"Remove\" to remove the products from the basket.
-        » Write \"Buy\" to make the purchase.''')
-label3.pack()
+        » Prass \"Add\" to add the product to the basket.
+        » Prass \"Remove\" to remove the products from the basket.
+        » Prass \"Buy\" to make the purchase.''')
+label3.pack(side=TOP)
 button_add = Button(frame_main, text="Add")
 button_add.config(command=add)
-button_add.pack()
+button_add.pack(side=TOP)
 button_remove = Button(frame_main, text="Remove")
 button_remove.config(command=remove)
-button_remove.pack()
+button_remove.pack(side=TOP)
 button_buy = Button(frame_main, text="Buy")
 button_buy.config(command=buy)
-button_buy.pack()
-lable4= Label(frame_main, text="Products available:")
-lable4.pack()
-label_products = Label(frame_main, text=show_products())
-label_products.pack()
-lable5= Label(frame_main, text="Products in your basket:")
-lable5.pack()
-label_basket = Label(frame_main, text=show_basket())
-label_basket.pack()
+button_buy.pack(side=TOP)
+label_no_buy = Label(frame_main, text="")
+label_no_buy.pack(side=TOP)
+
+frame_info = Frame(frame_main)
+frame_info.pack(side=TOP)
+
+frame_products = Frame(frame_info)
+frame_products.pack(side=LEFT)
+
+lable4= Label(frame_products, text="Products available:")
+lable4.pack(side=TOP)
+label_products = Label(frame_products, text=show_products())
+label_products.pack(side=TOP)
+
+frame_basket = Frame(frame_info)
+frame_basket.pack(side=LEFT)
+
+lable5= Label(frame_basket, text="Products in your basket:")
+lable5.pack(side=TOP)
+label_basket = Label(frame_basket, text=show_basket())
+label_basket.pack(side=TOP)
 
 
-window_start.mainloop()
+#Buy choice frame 2
+label6= Label(frame_buy_choice2, text=f"")
+label6.pack(side=TOP)
+button_yes2 = Button(frame_buy_choice2, text="Yes", command=yes_move2)
+button_yes2.pack(side=TOP)
+button_no2 = Button(frame_buy_choice2, text="No", command=no_move2)
+button_no2.pack(side=TOP)
 
 
-#all separation_line function
-#separation_line()
-#user_name=str(input("Please enter your name for personalised greeting: "))
-#separation_line()
-#purchasing_choice=str(input(f"Hi {user_name}\nWould you like to make a purchase?\nYes or No: ")).lower()
-#checks if the user wants to use the application
-#while True:
-    #if purchasing_choice=="yes":
-       # print("Ok",user_name)
-        #call products_list function
-        #main()
-        #sets total_final_final to total_final
-        #total_final_final=total_store[0]
-        #print(f"Final bill:{str(total_final_final)}$")
-        #print("Thank you for your purchase")
-       #break
-    #elif purchasing_choice=="no":
-        #print("Ok, bye", user_name)
-        #break
-   # else:
-        #print("Error, invalid input")
-        #purchasing_choice=str(input("Please enter Yes or No: ")).lower()
-#print("END")
+#Discount frame
+label7= Label(frame_discount, text='''Your promo cods:
+              
+-------------------------------------------------------
+⟫ -50% promo code: HALF
+-------------------------------------------------------
+
+
+-------------------------------------------------------
+⟫ -???% promo code: RANDOM
+-------------------------------------------------------
+
+
+-------------------------------------------------------
+⟫ your own % promo code: YOUR
+-------------------------------------------------------
+
+
+-------------------------------------------------------
+What is your promo cod?''')
+label7.pack(side=TOP)
+entry_discount = Entry(frame_discount, bg='lightgray')
+entry_discount.pack(side=TOP)
+button_discount = Button(frame_discount, text="Apply", command=discount)
+button_discount.pack(side=TOP)
+label_discount = Label(frame_discount, text="")
+label_discount.pack()
+
+
+#End frame
+label8= Label(frame_end, text="Thank you for your purchase")    
+label8.pack(side=TOP)
+label9= Label(frame_end, text="")
+label9.pack(side=TOP)
+button_close = Button(frame_end, text="Close", command=window_app.destroy)
+button_close.pack(side=TOP)
+
+show_frame(frame_start)
+
+window_app.mainloop()
--- work_func.py
+++ work_func.py
@@ -6,10 +6,6 @@ prices=[3.5, 4.0, 2.0, 1.5, 1.0, 2.5, 5.0, 0.8, 3.0, 1.2]
 num_items=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
-def separation_line():
-    #Outputs 55 -
-    print("-"*55)
-
 def show_products():
     msg=f"\n{'Num. of items:':<15}\t{'Items:':<15}\t\t{'Price:':<10}\n"
     for counter in range(len(products)):
@@ -24,6 +20,7 @@ def show_basket():
     msg +="Total: "+str(total())+"$"
     return msg
 
+
 def add_to_basket(name, amount):
     #Adds the amount of product to the basket
     if name in products:
@@ -35,32 +32,36 @@ def add_to_basket(name, amount):
     
     return msg
 
+def remove_from_basket(name, amount):
+    #Removes the amount of product from the basket
+    if name in products:
+            if num_items[products.index(name)] >= amount:
+                num_items[products.index(name)] -= amount
+                msg = f"{amount} {name} removed from the basket"  
+            else:
+                msg = "Error, you are removing more than you have"
+    else:
+        msg = "Error, invalid product"
+    
+    return msg
 
 def total():
     total=0
     for counter in range(len(products)):
         total+=(prices[counter]*num_items[counter])
     return total
-
-def your_d_code():
-    user_percentage=input("Please enter whatever percentage discount you want as just a number: ")
-            #While loop to make sure that user_percentage is integer and in between 0 and 100
-    while True:
-        # Check if input is a positive integer
-        if user_percentage.isdigit():
-            # sets user_percentage to integer
-            user_percentage = int(user_percentage)
-            while True:
-                if user_percentage>=0 and user_percentage<=100:
-                    user_percentage/=100
-                    user_percentage= 1 - user_percentage
-                    break
-                else:
-                    print("Error, invalid percentage discount")
-                    user_percentage=int(input("Please enter percentage discount between 0 and 100 as just a number: "))
-            break
+ 
+def your_d_code(user_percentage):
+    # Check if input is a positive integer
+    if user_percentage.isdigit():
+        # sets user_percentage to integer
+        user_percentage = int(user_percentage)
+        if user_percentage>=0 and user_percentage<=100:
+            user_percentage/=100
+            user_percentage= 1 - user_percentage
+            msg = f"Discount applied: {user_percentage*100}%"
         else:
-            print("Error, invalid data type") 
-            user_percentage=input("Please enter your percentage as just a whole number: ")
-    discount*=user_percentage
-    return discount
+            msg = "Error, invalid percentage discount"
+    else:
+        msg = "Error, invalid data type"
+    return user_percentage, msg
